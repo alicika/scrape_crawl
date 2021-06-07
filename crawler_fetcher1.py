7a9d106f65ad2c0334a0e0b4fe9827b68c7a69bf
alicika <52126032+alicika@users.noreply.github.com>
2021-06-07 14:45:08 +0000

    format and aggregate lines of code to use generator

--- crawler_fetcher1.py
+++ crawler_fetcher1.py
@@ -1,8 +1,15 @@
 import requests
 import lxml.html
 
+def scrape_list_page(res):
+    root = lxml.html.fromstring(res.content)
+    root.make_links_absolute(res.url)
+
+    for a in root.cssselect('#listbook a[itemprop="url"]'):
+        url = a.get('href')
+        yield(url)
+
 res = requests.get('https://sample.scraping-book.com/dp/')
-root = lxml.html.fromstring(res.content)
-for a in root.cssselect('a[itemprop="url"]'):
-    url = a.get('href')
-    print(url)
+url_set = scrape_list_page(res)
+for u in url_set:
+    print(u)
