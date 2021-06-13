8d4fed689d3a9e73fa452f64b3ed11fa47de5c85
alicika <52126032+alicika@users.noreply.github.com>
2021-06-13 23:16:23 +0900

    locate a client of MongoDB

--- crawler_fetcher.py
+++ crawler_fetcher.py
@@ -20,10 +20,15 @@ def scrape_detail_page(res):
     ebook = {
         'url': res.url,
         'title': root.cssselect('#bookTitle')[0].text_context(),
-        'price': root.cssselect('.buy')[0].text(),
-        'content': [h3.text_content() for h3 in root.cssselect('#content > h3')],
+        'price': root.cssselect('.buy')[0].text.strip(),
+        'content': [norm_spaces(h3.text_content()) for h3 in root.cssselect('#content > h3')],
         }
     return ebook
+
+
+def extract_key(url):
+    m = re.search(r'/([^/]+)$', url)
+    return m.group(1)
 
 
 def norm_spaces(res):
@@ -31,13 +36,20 @@ def norm_spaces(res):
 
 
 def main():
-    session = requests.Session()
+    client = MongoClient('localhost', 27017)
+    collection = client.scraping.ebooks
+    collection.create_index('key', unique=True)
+
     res = requests.get('https://gihyo.jp/dp')
     url_set = scrape_list_page(res)
     for u in url_set:
-        time.sleep(1)
-        res = session.get(u)
-        ebook = scrape_detail_page(res)
+        key = extract_key(u)
+        ebook = collection.find_one({'key': key})
+        if not ebook:
+            time.sleep(1)
+            res = requests.get(u)
+            ebook = scrape_detail_page(res)
+            collection.insert_one(ebook)
         print(ebook)
 
 
