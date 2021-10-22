ce3ea5e6f4d8ce80f1e5d85f6f59b7635f50d6e6
alicika <52126032+alicika@users.noreply.github.com>
2021-10-22 22:47:13 +0900

    implement minimum api query

---
+++ youtube_api.py
@@ -0,0 +1,15 @@
+import os
+from apiclient.discovery import build
+
+YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
+
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+search_response = youtube.search().list(
+    part="snippet",
+    q="YouTubeAPI",
+    type="video",
+).execute()
+
+# outputs video title
+for item in search_response["items"]:
+    print(item["snippet"]["title"])
