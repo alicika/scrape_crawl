0e860d85186c69ebc22f0642090d4c13f1363a06
alicika <52126032+alicika@users.noreply.github.com>
2021-10-27 02:35:33 +0900

    implement main functionality

--- youtube_data_store.py
+++ youtube_data_store.py
@@ -6,16 +6,56 @@ from googleapiclient.discovery import build
 from pymongo import MongoClient, ReplaceOne, DESCENDING
 from pymongo.collection import Collection
 
-YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
+YOUTUBE_API_KEY = "AIzaSyCx0TBDRRE_ALey-w6aYNUTXQLo1HPKHds"
+
 logging.getLogger("apiclient.discovery_cache").setLevel(logging.WARNING)
 
 
-def search_videos(param):
-    pass
+def search_videos(param: str, max_pages: int = 5) -> Iterator[List[dict]]:
+    """
+    :param param:
+    :param max_pages:
+    :return:
+    returns a list of items by page with 'param' arguments, up to max_pages.
+    """
+    youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+    search_request = youtube.search().list(
+        part="id",
+        q=param,
+        type="video",
+        maxResults=50,
+    )
+
+    i = 0
+    while search_request and i < max_pages:
+        search_response = search_request.execute()
+        video_ids = [item["id"]["videoId"] for item in search_response["items"]]
+
+        videos_response = youtube.videos().list(
+            part="snippet,statistics",
+            id=','.join(video_ids),
+        ).execute()
+
+        yield videos_response["items"]
+        search_request = youtube.search.list_next(search_request, search_response)
+        i += 1
 
 
-def save_to_mongodb(collection, item):
-    pass
+def save_to_mongodb(collection: Collection, items: List[dict]):
+    for item in items:
+        item["_id"] = item["id"]
+
+        for key, value in item["statistics"].items():
+            item["statistics"][key] = int(value)
+
+    operations = [ReplaceOne({"_id": item["_id"]}, item, upsert=True) for item in items]
+    result = collection.bulk_write(operations)
+    logging.info(f"upserted {result.upserted_count} documents")
+
+
+def show_top_videos(collection: Collection, top: int):
+    for item in collection.find().sort("statistics.viewCount", DESCENDING).limit(top):
+        print(item["statistics"]["viewCount"], item["snippet"]["title"])
 
 
 def main():
@@ -24,6 +64,9 @@ def main():
 
     for item in search_videos("YOUTUBEAPI"):
         save_to_mongodb(collection, item)
+
+    show_top_videos(collection, 5)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
