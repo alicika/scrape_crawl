5614cb94947b4102c0d110b83a86ea9da93ff7bc
alicika <52126032+alicika@users.noreply.github.com>
2021-11-03 23:44:57 +0900

    read csv and plot it into a graph

--- forex_etc.py
+++ forex_etc.py
@@ -4,7 +4,17 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def main():
-    pass
+    df_exchange = pd.read_csv(
+        'exchange.csv', encoding='cp932', header=1, names=['date', 'USD', 'rate'],
+        index_col=0, parse_dates=True)
+
+    min_date = datetime(1973, 1, 1)
+    max_date = datetime.now()
+    plt.subplot(1, 1, 1)
+    plt.plot(df_exchange.index, df_exchange.USD, label='ドル・円')
+    plt.xlim(min_date, max_date)
+    plt.ylim(50, 250)
+    plt.legend(loc='best')  
 
 
 def parse_year_and_month(year: str, month: str) -> datetime:
